5add9970dde79a4a3c1202d0299b3e82fb27b601
Cody Wishart <cody.wishart@gmail.com>
2021-01-27 15:45:44 -0600

    Create Challenge2.sh

---
+++ Challenge2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script Name:                  Challenge2 
+# Class Name:                   Ops 201
+# Author Name:                  Cody Wishart
+# Date of latest revision:      1/27/21
+# Purpose:                      Output log history
+
+log="last"
+
+Log_History () {
+    $log
+}
+
+Log_History
