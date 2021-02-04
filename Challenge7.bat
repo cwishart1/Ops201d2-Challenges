80bf3b05944dcaa95618bb98a639a476e32d40fe
Cody Wishart <cody.wishart@gmail.com>
2021-02-03 20:08:19 -0600

    Update Challenge7.bat

--- Challenge7.bat
+++ Challenge7.bat
@@ -1,6 +1,8 @@
-
-
-@echo off
+:: Script Name:                  Challenge7 
+:: Class Name:                   Ops 201
+:: Author Name:                  Cody Wishart
+:: Date of latest revision:      2/3/21
+:: Purpose:                      Backup files
 
 ROBOCOPY "C:\Users\l.thompson\Desktop\My Project Files" "C:\Users\j.thompson\Documents\Backup" /LOG:backuplog.txt
 
